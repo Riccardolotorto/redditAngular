753e5123f5b94426f6d9ae75f68b6f776e6df92e
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-02-19 11:40:40 +0000

    completo sort

--- reddit-angular/src/app/app.component.ts
+++ reddit-angular/src/app/app.component.ts
@@ -23,4 +23,9 @@ export class AppComponent {
     link.value = '';
   return false;
   }
+
+  //mostro gli articoli in base ai voti: ordine decrescente
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }  
 }
